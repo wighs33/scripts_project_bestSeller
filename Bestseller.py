d823267d45a95b9422e6d272eb072c420b9d1112
rkdlqzz <rkdlqzz081@gmail.com>
2021-05-26 18:07:21 +0900

    Update Bestseller.py
    하단 메뉴(홈, 검색, 즐겨찾기, 도서관) 버튼 생성

--- Bestseller.py
+++ Bestseller.py
@@ -4,13 +4,34 @@ from tkinter import font
 ################################################################
 # home
 ################################################################
+def menuHome():
+    pass
+def menuSearch():
+    pass
+def menuFavorites():
+    pass
+def menuLibrary():
+    pass
 def Init_topLabel():
     font_ = font.Font(window, size=30, weight='bold', family='Consolas')
     topLabel = Label(window, text='Bestseller', font=font_)
     topLabel.place(x=185, y=20)
-
+def Init_menuButton():
+    font_ = font.Font(window, size=20, weight='bold', family='Consolas')
+    b_width, b_height = 10, 2
+    b_x, b_y = 0, 662
+    b_menu = []
+    b_menu.append(Button(window, text="홈", command=menuHome, font=font_, width=b_width, height=b_height))
+    b_menu.append(Button(window, text="검색", command=menuSearch, font=font_, width=b_width, height=b_height))
+    b_menu.append(Button(window, text="즐겨찾기", command=menuFavorites, font=font_, width=b_width, height=b_height))
+    b_menu.append(Button(window, text="도서관", command=menuLibrary, font=font_, width=b_width, height=b_height))
+    b_menu[0].place(x=b_x, y=b_y)
+    b_menu[1].place(x=b_x+150, y=b_y)
+    b_menu[2].place(x=b_x+300, y=b_y)
+    b_menu[3].place(x=b_x+450, y=b_y)
 def Init_HomeState():
     Init_topLabel()
+    Init_menuButton()
 
 window = Tk()
 window.title('Bestseller')
