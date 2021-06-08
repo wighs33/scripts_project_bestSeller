c695d6cb4023340d27bc2d694a60db3c74a45493
rkdlqzz <rkdlqzz081@gmail.com>
2021-06-08 10:11:01 +0900

    setup 파일

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='BESTSELLER',
+      version='1.0',
+      py_modules=['Bestseller', 'book', 'func', 'gmail', 'library', 'oepnAPI', 'telegram']
+      )
